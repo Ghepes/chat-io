8c1174512f33c0c1541aee312dd3f99bce4664ba
Ersin84 <info.wromo@gmail.com>
2023-03-26 22:54:25 +0200

    add

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+  console.log('a user connected');
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at https://dev-backend.vercel.app/`);
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
+
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+      io.emit('chat message', msg);
+    });
+  });
+  
+//  http.listen(port, () => {
+//    console.log(`Socket.IO server running at http://localhost:${port}/`);
+//  });
